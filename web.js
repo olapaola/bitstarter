1abb1cf00376febf9ef89951ab85e66702b39505
olapaola <olapaola@gmail.com>
2013-07-08 18:33:25 +0000

    Update web.js again

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
